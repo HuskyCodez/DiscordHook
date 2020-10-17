169f0375e7112abfb4582eb6f6da1c341e26ee7b
HuskyCodez <jk.temp2018@gmail.com>
2020-10-17 17:15:24 +0800

    e

--- main.py
+++ main.py
@@ -7,22 +7,30 @@ sent = 1
 def Spammer(webhook):
     rcolor = randint(1, 16777215)
     global sent
-    content={
-       "username": user,
-       "avatar_url": "https://toppng.com/uploads/preview/nuke-png-11553998200wx5xfo50cx.png",
-       "embeds": [
-           {
-                "author": {
-                    "username": "NUKED LOL",
-                    "url": None,
-                    "icon_url": "https://toppng.com/uploads/preview/nuke-png-11553998200wx5xfo50cx.png"
-             },
-             "title": "FAT NOOB",
-             "description": spammsg,
-             "color": rcolor
+    if ping == 'y':
+        content={
+                "content": "@everyone",
+                "embeds": [
+                    {
+                    "title": "GET NUKED LOL",
+                    "description": spammsg,
+                    "color": 15746887
+                    }
+                ],
+                "username": user
+                }
+    elif ping == 'n':
+        content={
+            "embeds": [
+                {
+                "title": "GET NUKED LOL",
+                "description": spammsg,
+                "color": 15746887
+                }
+            ],
+            "username": user
             }
-        ]
-    }
+
     r = requests.post(webhook, json=content)
     if r.status_code == 204:
         lock.acquire()
@@ -33,8 +41,9 @@ def Spammer(webhook):
         pass
 
 if __name__ == "__main__":
-    ctypes.windll.kernel32.SetConsoleTitleW("Webhook spammer -HuskyCodez :)")
+    ctypes.windll.kernel32.SetConsoleTitleW("Webhook spammer - HuskyCodez :)")
     webhook = input('Webhook URL?: ')
+    ping = input('Ping everyone? (y/n): ')
     user = input("Webhook username?: ")
     spammsg = input("Text to spam?: ")
     print()
